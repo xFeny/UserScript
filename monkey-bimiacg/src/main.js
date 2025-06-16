aa2fddc1a8eaacd49dc34ed518c099342e1fd169
Feny <1220301855@qq.com>
2025-06-16 11:52:32 +0800

    修改为只保留切换路线功能

--- monkey-bimiacg/src/main.js
+++ monkey-bimiacg/src/main.js
@@ -1,16 +1,8 @@
 import "./style.css";
 import siteConfigs from "./siteConfigs";
 
-function getTitle() {
-  let title = document.title;
-  title = title.substring(0, title.lastIndexOf("|")).trim();
-  title = title.substring(0, title.lastIndexOf(" ")).trim();
-  return title;
-}
 const MSG_SOURCE = "FENY_SCRIPTS_ANIME";
 const settings = { volume: 1, opacity: 0.88, autoPlay: true, defaultWide: true };
-// https://www.moduzy7.com/search/-------------/?wd=${getTitle()}
-const searchAnime = `https://www.ezdmw.site/Index/search.html?searchText=${getTitle()}`;
 const App = {
   init() {
     this.videoSetting();
@@ -19,46 +11,15 @@ const App = {
   },
   setupMutationObserver() {
     const observer = new MutationObserver(() => {
-      this.autoPlay();
-      const ok = this.webFullScreen();
-      if (ok || this.video) observer.disconnect();
+      document.querySelector(".line_button2")?.remove();
     });
     observer.observe(document.body, { childList: true, subtree: true });
-    setTimeout(() => observer.disconnect(), 10000);
+    setTimeout(() => observer.disconnect(), 5000);
   },
-  query: (selector, context) => (context || document).querySelector(selector),
-  querys: (selector, context) => (context || document).querySelectorAll(selector),
   isFanTuan: () => location.host.includes("ft"),
   isEzSite: () => location.host.includes("ezdmw"),
   isBimi: () => location.host.includes("bimiacg"),
-  getFrame() {
-    return this.query("iframe:not([src=''], [src='#'], [id='buffer'], [id='install'])");
-  },
   postMessage: (data) => parent?.postMessage({ source: MSG_SOURCE, ...data }, "*"),
-  autoPlay() {
-    if (this.isBimi()) return;
-    this.video = this.query("video");
-    if (!this.video) return;
-    this.video.addEventListener("canplay", () => {
-      this.video.play().catch(() => this.postMessage({ reloadFrame: true }));
-    });
-  },
-  reloadVideoFrame() {
-    if (this.isReloaded) return;
-    const frame = this.getFrame();
-    frame.src = frame.src;
-    this.webFullScreen();
-    this.isReloaded = true;
-  },
-  webFullScreen() {
-    if (location.pathname === "/") return true;
-    this.query(".line_button2")?.remove();
-    const frame = this.getFrame();
-    if (!frame) return false;
-    frame.setAttribute("allow", "autoplay");
-    frame.addEventListener("load", () => this.toggleClass());
-    return true;
-  },
   videoSetting() {
     settings.theme = this.isBimi() ? "bilibili" : "YouTube";
     const storage = JSON.parse(localStorage.getItem("html5Settings"));
@@ -67,16 +28,12 @@ const App = {
   setupKeydownListener() {
     window.addEventListener("keydown", (event) => {
       const hotKey = event.key.toUpperCase();
-      const tagName = event.target.tagName;
-      if (["INPUT", "TEXTAREA"].includes(tagName)) return;
+      if (["INPUT", "TEXTAREA"].includes(event.target.tagName)) return;
       if (window.top !== window) return this.postMessage({ hotKey }, "*");
       this.execHotKeyActions(hotKey);
     });
-    window.addEventListener("message", (event) => {
-      const { data } = event;
-      if (!data?.source) return;
-      if (!data.source.includes(MSG_SOURCE)) return;
-      if (data?.reloadFrame) return this.reloadVideoFrame();
+    window.addEventListener("message", ({ data }) => {
+      if (!data?.source?.includes(MSG_SOURCE)) return;
       if (data?.hotKey) this.execHotKeyActions(data.hotKey);
     });
   },
@@ -86,53 +43,26 @@ const App = {
   },
   getKeyMapping() {
     return {
-      V: () => (this.isBimi() ? GM_openInTab(searchAnime) : null),
-      F: () => this.toggleWebFullScreen(),
-      "[": () => this.switchEpisodes(true),
-      "]": () => this.switchEpisodes(),
       T: () => {
         // E站切换逻辑
         if (this.isEzSite()) {
-          const routes = siteConfigs.ezSite.getRoutes(this);
-          const currentRoute = siteConfigs.ezSite.getCurrentRoute(this);
+          const routes = siteConfigs.ezSite.getRoutes();
+          const currentRoute = siteConfigs.ezSite.getCurrentRoute();
           siteConfigs.ezSite.switchRoute(routes, currentRoute);
         }
         // 饭团动漫切换逻辑
         if (this.isFanTuan()) {
-          const currentRoute = siteConfigs.fanTuan.getCurrentRoute(this);
-          siteConfigs.fanTuan.switchRoute(currentRoute, this);
+          const currentRoute = siteConfigs.fanTuan.getCurrentRoute();
+          siteConfigs.fanTuan.switchRoute(currentRoute, siteConfigs);
         }
         // 哔咪动漫切换逻辑
         if (this.isBimi()) {
-          const routes = siteConfigs.bimi.getRoutes(this);
+          const routes = siteConfigs.bimi.getRoutes();
           const currentEpisode = siteConfigs.bimi.getCurrentEpisode();
-          siteConfigs.bimi.switchRoute(routes, currentEpisode, this);
+          siteConfigs.bimi.switchRoute(routes, currentEpisode, siteConfigs);
         }
       },
     };
-  },
-  toggleWebFullScreen() {
-    const player = this.query(":is(.player, .ty-play)");
-    window.scrollTo({ top: player?.getBoundingClientRect().top || 0 });
-    this.toggleClass();
-  },
-  switchEpisodes(isPrev = false) {
-    if (this.isBimi()) return this.query(isPrev ? ".pre" : ".next")?.click();
-    const episode = this.query(`a[href="${location.pathname}"]`);
-    if (this.isEzSite()) return (isPrev ? episode?.nextElementSibling : episode?.previousElementSibling).click();
-    (isPrev ? episode?.previousElementSibling : episode?.nextElementSibling).click();
-  },
-  index(element) {
-    if (!element) return;
-    const parentEle = element.parentElement;
-    if (!parentEle) return -1;
-    const children = Array.from(parentEle.children);
-    return children.indexOf(element);
-  },
-  toggleClass() {
-    const selector = "webFullScreen";
-    this.getFrame()?.classList.toggle(selector);
-    this.query(".MacPlayer")?.classList.toggle(selector);
   },
 };
 
--- monkey-bimiacg/src/siteConfigs.js
+++ monkey-bimiacg/src/siteConfigs.js
@@ -1,36 +1,35 @@
-// 封装通用的获取元素列表的函数
-const queryElements = (self, selector) => Array.from(self.querys(selector));
-
 // 定义不同网站的切换路线配置
 export default {
+  index(element) {
+    const children = Array.from(element?.parentElement?.children ?? []);
+    return children.indexOf(element);
+  },
   // E站配置
   ezSite: {
-    getRoutes: (self) => queryElements(self, "div[class*='line_button']"),
-    getCurrentRoute: (self) => self.query("div[class*='line_button'][style*='rgb']"),
-    switchRoute: (routes, currentRoute) => {
-      routes.find((route) => route !== currentRoute)?.click();
-    },
+    getRoutes: () => Array.from(document.querySelectorAll("div[class*='line_button']")),
+    getCurrentRoute: () => document.querySelector("div[class*='line_button'][style*='rgb']"),
+    switchRoute: (routes, currentRoute) => routes.find((route) => route !== currentRoute)?.click(),
   },
   // 饭团动漫配置
   fanTuan: {
-    getCurrentRoute: (self) => self.query(".anime-episode.active"),
-    switchRoute: (currentRoute, self) => {
+    getCurrentRoute: () => document.querySelector(".anime-episode.active"),
+    switchRoute(currentRoute, self) {
       let nextRoute = currentRoute?.nextElementSibling;
       if (!nextRoute) nextRoute = currentRoute.parentElement.firstElementChild;
-      const index = self.index(self.query("a[class*='btn-episode active']"));
+      const index = self.index(document.querySelector("a[class*='btn-episode active']"));
       nextRoute.children[index].click();
     },
   },
   // 哔咪动漫配置
   bimi: {
-    getRoutes: (self) => queryElements(self, ".play_box"),
+    getRoutes: () => Array.from(document.querySelectorAll(".play_box")),
     getCurrentEpisode: () => document.querySelector(`a[href="${location.pathname}"]`)?.parentElement,
-    switchRoute: (routes, currentEpisode, self) => {
+    switchRoute(routes, currentEpisode, self) {
       const currentRouteIndex = routes.findIndex((route) => route.classList.contains("show"));
       let nextRouteIndex = currentRouteIndex + 1;
       if (nextRouteIndex >= routes.length) nextRouteIndex = 0;
       const currentEpisodeIndex = self.index(currentEpisode);
-      const episodes = self.querys("li", routes[nextRouteIndex]);
+      const episodes = routes[nextRouteIndex]?.querySelectorAll("li");
       episodes[currentEpisodeIndex]?.firstElementChild?.click();
     },
   },
