e9a99bc2ec3e9fccb45ebe58187a3f80b8f523b2
Feny <1220301855@qq.com>
2025-08-05 17:52:27 +0800

    更语义化命名

--- monkey-web-fullscreen/src/application/handler/WebFullEnhanceHandler.js
+++ monkey-web-fullscreen/src/application/handler/WebFullEnhanceHandler.js
@@ -71,12 +71,12 @@ export default {
     // 循环向上查找与初始元素宽高相等的父元素
     for (let parent = container, level = 0; parent && level < maxLevel; parent = parent.parentElement, level++) {
       if (parent.offsetWidth === cw && parent.offsetHeight === ch) container = parent;
-      if (this.hasExplicitSize(parent)) return container;
+      if (this.hasExplicitlySize(parent)) return container;
       this.videoAncestorElements.add(parent);
     }
     return container;
   },
-  hasExplicitSize(element) {
+  hasExplicitlySize(element) {
     const style = element.style;
     const sizeRegex = /^\d+(\.\d+)?(px|em|rem)$/;
     return ["width", "height"].some((prop) => {
