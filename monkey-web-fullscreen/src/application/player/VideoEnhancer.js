b74c29d7a50f85fd7d104a5ac7371d5ccc5ff468
Feny <1220301855@qq.com>
2025-08-08 13:08:13 +0800

    options.mode

--- monkey-web-fullscreen/src/application/player/VideoEnhancer.js
+++ monkey-web-fullscreen/src/application/player/VideoEnhancer.js
@@ -130,7 +130,7 @@ export default class VideoEnhancer {
       const shadowRoot = (this._shadowRoot = this._attachShadow.call(this, { ...options, mode: "open" }));
 
       // 模拟closed模式，防止针对性检测
-      Object.defineProperty(this, "shadowRoot", { configurable: true, get: () => null });
+      if (options.mode === "closed") Object.defineProperty(this, "shadowRoot", { configurable: true, get: () => null });
 
       // 自定义事件
       const shadowEvent = new CustomEvent("shadow-attached", { bubbles: true, detail: { shadowRoot } });
