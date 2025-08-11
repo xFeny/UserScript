f8f549278e616369598f5f8d2408fcea044af865
Feny <1220301855@qq.com>
2025-08-11 21:48:07 +0800

    注释掉App.autoNextEpisode(this);

--- monkey-web-fullscreen/src/application/player/VideoEventsHandler.js
+++ monkey-web-fullscreen/src/application/player/VideoEventsHandler.js
@@ -15,7 +15,7 @@ export default {
   timeupdate() {
     if (isNaN(this.duration)) return;
     App.autoWebFullscreen(this);
-    App.autoNextEpisode(this);
+    // App.autoNextEpisode(this);
     App.cachePlayTime(this);
   },
   canplay() {
