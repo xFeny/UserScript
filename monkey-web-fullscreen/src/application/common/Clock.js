dd1f9912d756c5c7f2721e051f4147074124f986
Feny <1220301855@qq.com>
2025-08-17 15:26:32 +0800

    视频全屏时显示时钟

--- monkey-web-fullscreen/src/application/common/Clock.js
+++ monkey-web-fullscreen/src/application/common/Clock.js
@@ -1,15 +1,19 @@
 /**
  * 实时显示当前时间
- * new Clock(video.parentElement);
  */
 export default class Clock {
   constructor(container) {
+    if (!container) return;
+
     this.container = container;
+    this.animationId = null;
+    this.isRunning = false;
+
     if (container.querySelector("clock")) return;
     this.clock = document.createElement("div");
-    this.clock.style = "top:15px;right:25px;font-size:18px;color:#eee;position:absolute";
+    this.clock.style = "top:20px;right:50px;font-size:18px;color:#FFF;position:absolute;";
     this.container.append(this.clock);
-    this.update();
+    this.start(); // 初始化时启动时钟
   }
 
   formatTime(date) {
@@ -20,8 +24,32 @@ export default class Clock {
   }
 
   update() {
+    this.isRunning = true;
     const now = new Date();
     this.clock.textContent = this.formatTime(now);
-    requestAnimationFrame(() => this.update());
+    this.animationId = requestAnimationFrame(() => this.update());
+  }
+
+  start() {
+    if (this.isRunning) return;
+    this.clock.style.setProperty("display", "unset");
+    this.update();
+  }
+
+  stop() {
+    if (this.animationId) {
+      cancelAnimationFrame(this.animationId);
+      this.animationId = null;
+    }
+
+    this.isRunning = false;
+    this.clock.style.display = "none";
+  }
+
+  destroy() {
+    this.stop();
+    this.clock?.remove();
+    this.container = null;
+    this.clock = null;
   }
 }
