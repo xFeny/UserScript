59cbe91ee449a4a636780159189fed80d57e1b56
Feny <1220301855@qq.com>
2025-08-22 01:06:39 +0800

    支持远程加载@match网站的点击图标配置

--- monkey-web-fullscreen/src/application/common/SiteIcons.js
+++ monkey-web-fullscreen/src/application/common/SiteIcons.js
@@ -1,15 +1,26 @@
 /**
  * 脚本`@match`的网站 网页全屏、弹幕、下一集css选择器配置
  */
-export default {
+const IconsSelector = {
   name: { full: "full", webFull: "webFull", next: "next", danmaku: "danmaku" },
-  "live.acfun.cn": { webFull: ".fullscreen-web", danmaku: ".danmaku-enabled" },
-  "www.bilibili.com": { webFull: ".bpx-player-ctrl-web", next: ".bpx-player-ctrl-next" },
-  "tv.sohu.com": { webFull: ".x-pagefs-btn", danmaku: ".tm-tmbtn", next: ".x-next-btn" },
   "v.douyu.com": { webFull: ".ControllerBar-PageFull-Icon", danmaku: ".BarrageSwitch-icon" },
-  "v.qq.com": { webFull: ".txp_btn_fake", danmaku: ".barrage-switch", next: ".txp_btn_next_u" },
   "v.youku.com": { webFull: "#webfullscreen-icon", danmaku: "#barrage-switch", next: ".kui-next-icon-0" },
   "www.acfun.cn": { webFull: ".fullscreen-web", danmaku: ".danmaku-enabled", next: ".btn-next-part div" },
+  "live.acfun.cn": { full: ".fullscreen-screen", webFull: ".fullscreen-web", danmaku: ".danmaku-enabled" },
   "www.mgtv.com": { webFull: ".webfullscreenBtn i", danmaku: "div[class*='danmuSwitch']", next: ".icon-next" },
+  "tv.sohu.com": { full: ".x-fullscreen-btn", webFull: ".x-pagefs-btn", danmaku: ".tm-tmbtn", next: ".x-next-btn" },
   "www.iqiyi.com": { webFull: "[class*=videofullBtn]", danmaku: "[class*=danmuBtnSet] div", next: "[class*=playNext]" },
+  "www.bilibili.com": { full: ".bpx-player-ctrl-full", webFull: ".bpx-player-ctrl-web", next: ".bpx-player-ctrl-next" },
+  "v.qq.com": { full: ".txp_btn_fullscreen", webFull: ".txp_btn_fake", danmaku: ".barrage-switch", next: ".txp_btn_next_u" },
 };
+
+export default await (async () => {
+  let remoteSelector = {};
+  try {
+    const res = await GM.xmlHttpRequest({ url: "https://gitee.com/xfeny/static-files/raw/master/IconsSelector.json" });
+    remoteSelector = JSON.parse(res.responseText);
+  } catch (e) {
+    console.error("加载远程配置失败", e);
+  }
+  return { ...IconsSelector, ...remoteSelector };
+})();
