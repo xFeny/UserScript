f84e9f76bf62c8329c1f5d3c837ab4de0bdae2c8
Feny <1220301855@qq.com>
2025-09-11 19:02:18 +0800

    修改版本号

--- monkey-web-fullscreen/vite.config.js
+++ monkey-web-fullscreen/vite.config.js
@@ -59,7 +59,7 @@ export default defineConfig({
         match,
         include,
         author: "Feny",
-        version: "3.3.1",
+        version: "3.3.2",
         connect: "gitee.com",
         license: "GPL-3.0-only",
         description: description.join("；"),
