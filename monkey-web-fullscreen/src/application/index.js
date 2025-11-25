be996090c2b205dd6783376135e1785644fa36f7
Feny <1220301855@qq.com>
2025-11-25 12:41:59 +0800

    modify

--- monkey-web-fullscreen/src/application/index.js
+++ monkey-web-fullscreen/src/application/index.js
@@ -3,15 +3,15 @@ import Keydown from "./handler/KeydownHandler";
 import Control from "./handler/VideoControlHandler";
 import WebFull from "./handler/WebFullscreenHandler";
 import Automatic from "./handler/AutoExecuteHandler";
-import Switch from "./handler/EpisodeSwitchHandler";
-import Picker from "./handler/EpisodePickerHandler";
+import Episode from "./handler/EpisodeSwitchHandler";
+import EpisodePicker from "./handler/EpisodePickerHandler";
 import SettMsg from "./handler/SettMessageHandler";
 import Extend from "./handler/VideoExtendHandler";
 import Ignore from "./handler/IgnoreUrlsHandler";
 import Login from "./handler/WebLoginHandler";
 import Menu from "./handler/MenuHandler";
 
-const handlers = [Listen, Keydown, Control, WebFull, Automatic, Switch, Picker, Ignore, SettMsg, Menu, Extend, Login];
+const handlers = [Listen, Keydown, Control, WebFull, Automatic, Episode, EpisodePicker, Ignore, SettMsg, Menu, Extend, Login];
 unsafeWindow.AUTO_WEB_FULLSCREEN = window.App = {};
 handlers.forEach((handler) => {
   // 将Handler方法变为App的方法，内部this指向App
