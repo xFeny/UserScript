3b815f3fc36939b899d72857f8062fa0fe7c815b
Feny <1220301855@qq.com>
2025-11-26 21:22:03 +0800

    优化

--- monkey-web-fullscreen/src/application/handler/VideoExtendHandler.js
+++ monkey-web-fullscreen/src/application/handler/VideoExtendHandler.js
@@ -43,7 +43,7 @@ export default {
     return unsafeWindow.webPlay?.wonder?._player?._playProxy?._info?.duration ?? video.duration;
   },
   videoProgress(video) {
-    if (!video || this.isBackgroundVideo(video)) return;
+    if (!video || this.player !== video || this.isBackgroundVideo(video)) return;
     if (video.duration <= 30 || this.isLive() || this.shouldDestroyTimeElement()) return this.removeVideoProgress();
 
     if (!this.progressElement) {
