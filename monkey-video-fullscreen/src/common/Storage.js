f6f1da7a75bbffc6aeb3ba59ec1afa9a05466ee2
Feny <1220301855@qq.com>
2025-12-16 16:18:02 +0800

    基础存储类修改

--- monkey-video-fullscreen/src/common/Storage.js
+++ monkey-video-fullscreen/src/common/Storage.js
@@ -1,13 +1,13 @@
-import { StorageItem, SuffixStorage } from "../lib/StorageUtils";
+import BasicStorage from "../lib/BasicStorage";
 
 /**
  * 脚本存储键值配置
  */
 export default unsafeWindow.FyStorage = {
-  /** 网页全屏相关：自定义此站视频容器选择器 */
-  CUSTOM_CONTAINER: new SuffixStorage("CUSTOM_CONTAINER", ""),
+  /** 网页全屏相关：自定义此站视频容器 */
+  CUSTOM_CONTAINER: new BasicStorage("CUSTOM_CONTAINER_", "", false, undefined, true),
   /** 网页全屏相关：此站启/禁用自动网页全屏 */
-  THIS_SITE_AUTO: new SuffixStorage("THIS_SITE_AUTO_", false, false, Boolean),
+  THIS_SITE_AUTO: new BasicStorage("THIS_SITE_AUTO_", false, false, Boolean, true),
   /** 网页全屏相关：自动网页全屏忽略的网址 */
-  IGNORE_URLS: new StorageItem("IGNORE_URLS", ""),
+  IGNORE_URLS: new BasicStorage("IGNORE_URLS", ""),
 };
