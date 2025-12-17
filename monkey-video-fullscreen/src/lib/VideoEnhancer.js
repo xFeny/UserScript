b051756b45af30912a5f3bbce06a6fff625cbccf
Feny <1220301855@qq.com>
2025-12-17 11:13:09 +0800

    优化

--- monkey-video-fullscreen/src/lib/VideoEnhancer.js
+++ monkey-video-fullscreen/src/lib/VideoEnhancer.js
@@ -1,38 +1,6 @@
 import Tools from "../common/Tools";
 
 class VideoEnhancer {
-  static setPlaybackRate(video, playRate) {
-    this.defineProperty(video, "playbackRate", {
-      set(value, setter) {
-        if (this.playbackRate === value) return;
-        this.__playRate === value && setter(value);
-      },
-    });
-    video.playbackRate = video.__playRate = (+playRate).toFixed(2).replace(/\.?0+$/, "");
-  }
-
-  static defineProperty(video, property, descs) {
-    try {
-      const isMediaElement = video instanceof HTMLMediaElement;
-      const videoPrototype = isMediaElement ? HTMLMediaElement.prototype : Object.getPrototypeOf(video);
-      const original = Object.getOwnPropertyDescriptor(videoPrototype, property);
-      if (!original) throw new Error(`属性 ${property} 不存在`);
-
-      Object.defineProperty(isMediaElement ? video : videoPrototype, property, {
-        get() {
-          return descs.get ? descs.get.call(this, original.get.call(this)) : original.get.call(this);
-        },
-        set(value) {
-          const oldVal = original.get.call(this);
-          descs.set ? descs.set.call(this, value, original.set.bind(this), oldVal) : original.set.call(this, value);
-        },
-        configurable: true,
-      });
-    } catch (e) {
-      console.error(`Modify ${property} Error: `, e);
-    }
-  }
-
   static hackAttachShadow() {
     if (Element.prototype.__attachShadow) return;
     Element.prototype.__attachShadow = Element.prototype.attachShadow;
@@ -54,8 +22,10 @@ class VideoEnhancer {
     if (!videos.length) return;
 
     videos.forEach((video) => {
+      const root = video.getRootNode();
+      if (!(root instanceof ShadowRoot)) return;
       Tools.emitEvent("shadow-video", { video });
-      Tools.emitEvent("addStyle", { shadowRoot: video.getRootNode() });
+      Tools.emitEvent("addStyle", { shadowRoot: root });
     });
   }
 }
