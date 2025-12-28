88c3254cfa671dc2f55566b30dda048d4f5d49d5
Feny <1220301855@qq.com>
2025-12-28 14:36:48 +0800

    快速跳过爱奇艺"关闭拦截插件并刷新页面"的倒计时提示

---
+++ quickly-skip-hints.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         快速跳过提示
+// @namespace    https://docs.scriptcat.org/
+// @version      0.1.0
+// @license      MIT
+// @description  快速跳过爱奇艺"关闭拦截插件并刷新页面"的倒计时提示
+// @author       Feny
+// @match        https://www.iqiyi.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.iqiyi.com
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const nativeSetInterval = unsafeWindow.setInterval;
+    unsafeWindow.setInterval = function (callback, delay) {
+        const include = callback.toString().includes("showBlackScreen")
+        return nativeSetInterval.call(this, callback, include ? 5 : delay);
+    };
+
+})();
