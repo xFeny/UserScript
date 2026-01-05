b398a2af01e560e94185570a739184c969c85abe
Feny <1220301855@qq.com>
2026-01-05 13:48:42 +0800

    modify

--- monkey-video-fullscreen/src/handler/IgnoreUrlsHandler.js
+++ monkey-video-fullscreen/src/handler/IgnoreUrlsHandler.js
@@ -4,11 +4,9 @@ import Storage from "../common/Storage";
  * 处理：自动网页全屏时忽略的网址
  */
 export default {
+  initIgnoreUrls: () => (App.urlFilter = App.getIgnoreUrls()),
   setupIgnoreUrlsChangeListener() {
     GM_addValueChangeListener(Storage.IGNORE_URLS.name, (_, oldVal, newVal) => oldVal !== newVal && this.initIgnoreUrls());
-  },
-  initIgnoreUrls() {
-    this.urlFilter = this.getIgnoreUrls();
   },
   isIgnoreUrl() {
     if (!this.urlFilter) this.initIgnoreUrls();
