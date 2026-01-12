2052898561c016e84232360e8380ba71e2c74186
Feny <1220301855@qq.com>
2026-01-12 23:51:40 +0800

    modify version

--- monkey-video-fullscreen/vite.config.js
+++ monkey-video-fullscreen/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
       entry: "src/main.js",
       userscript: {
         author: "Feny",
-        version: "3.8.8",
+        version: "3.8.9",
         match: ["*://*/*"],
         license: "GPL-3.0-only",
         name: "视频网页全屏",
