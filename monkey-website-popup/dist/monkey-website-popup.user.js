a220edab40e5c12312c2cf7f01dd9b8f70296510
Feny <1220301855@qq.com>
2026-01-21 23:37:47 +0800

    调整

--- monkey-website-popup/dist/monkey-website-popup.user.js
+++ monkey-website-popup/dist/monkey-website-popup.user.js
@@ -19,14 +19,14 @@
   'use strict';
 
   const App = {
+    getValue: () => GM_getValue(location.host),
+    setValue: (value) => GM_setValue(location.host, value),
     addStyle(id = "gm_hide_some") {
       const cssText = "width:0!important;height:0!important;opacity:0!important;display:none!important;";
       const value = this.getValue();
       document.querySelector(`#${id}`)?.remove();
       value && GM_addElement("style", { id, textContent: `${value}{${cssText}}` });
     },
-    getValue: () => GM_getValue(location.host),
-    setValue: (value) => GM_setValue(location.host, value),
     setupMenuCmds() {
       const title = "此站要隐藏的元素";
       GM_registerMenuCommand(title, () => {
--- monkey-website-popup/src/main.js
+++ monkey-website-popup/src/main.js
@@ -1,4 +1,6 @@
 const App = {
+  getValue: () => GM_getValue(location.host),
+  setValue: (value) => GM_setValue(location.host, value),
   addStyle(id = "gm_hide_some") {
     const cssText = "width:0!important;height:0!important;opacity:0!important;display:none!important;";
 
@@ -6,8 +8,6 @@ const App = {
     document.querySelector(`#${id}`)?.remove();
     value && GM_addElement("style", { id, textContent: `${value}{${cssText}}` });
   },
-  getValue: () => GM_getValue(location.host),
-  setValue: (value) => GM_setValue(location.host, value),
   setupMenuCmds() {
     const title = "此站要隐藏的元素";
     GM_registerMenuCommand(title, () => {
